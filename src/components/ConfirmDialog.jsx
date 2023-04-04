b47f20f8379d49ffbaca0291adf464feabe0d5ca
Sid Natarajan <sid.nat@gmail.com>
2023-04-04 18:25:09 -0400

    clean up

--- src/components/ConfirmDialog.jsx
+++ src/components/ConfirmDialog.jsx
@@ -1,40 +1,25 @@
-import { useState } from "react";
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  increment,
-  decrement,
-  isCompleted,
-  selectTrails
-} from '../features/trailsSlice';
 import {
   Button,
-  TextField,
+  // TextField,
   Dialog,
-  DialogActions,
-  DialogContent,
-  Select,
-  MenuItem,
+  // DialogActions,
+  // DialogContent,
+  // Select,
+  // MenuItem,
   Typography,
   Grid,
-  Card,
-  InputLabel,
-  FormControl
+  // Card,
+  // InputLabel,
+  // FormControl
 } from "@mui/material";
-import instance from '../utils/axios'
 
 const ConfirmDialog = (props) => {
-  const dispatch = useDispatch();
   const {
     clickedTrailName,
     setClickedTrailName,
     setTrailCompleted,
     trail
   } = props
-  const [inputs, setInputs] = useState({
-    title: "",
-    category: "",
-    note: "",
-  });
 
   return (
     <Dialog open={clickedTrailName} onClose={() => setClickedTrailName(false)}>
@@ -45,7 +30,6 @@ const ConfirmDialog = (props) => {
         <Typography variant="h4" align="center">
           {trail.difficulty}
         </Typography>
-
       </Grid>
       <Button onClick={() => setTrailCompleted(trail.name)}>
         Completed
